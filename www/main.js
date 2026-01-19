4421c3e112a16cef9930c59f3cf960210e02d665
Ridd07 <pansuriyariddhi0@gmail.com>
2026-01-19 10:40:13 +0530

    first commit

---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('.text').textllate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bounceIn",
+        },
+        ouy: {
+            effect: "bounceOut",
+        },
+    });
+});
