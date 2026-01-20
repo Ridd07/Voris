f8c735515a80aadfd1abcd83e25d9b031f951c73
Ridd07 <pansuriyariddhi0@gmail.com>
2026-01-20 21:51:47 +0530

    day 8

--- engine/command.py
+++ engine/command.py
@@ -1,18 +1,19 @@
 import pyttsx3
 import speech_recognition as sr
 import eel
-import sys
+import time
+
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 174)
-    print(voices, flush=True)
+    eel.DisplayMessage(text)
     engine.say(text)
     engine.runAndWait()
 
-@eel.expose
+
 def takecommand():
     r = sr.Recognizer()
     
@@ -20,9 +21,9 @@ def takecommand():
         print('listening....', flush=True)
         eel.DisplayMessage('listening....')
         r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source)
-        
-        audio = r.listen(source, 10, 6)
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r.listen(source, timeout=10, phrase_time_limit=None)
+
     
     try:
         print('recognizing', flush=True)
@@ -30,12 +31,26 @@ def takecommand():
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}", flush=True)
         eel.DisplayMessage(query)
-        speak(query)
-        eel.ShowHood()
-
-        return query
+        time.sleep(2)
     except Exception as e:
-        
         return ""
     
     return query.lower()
+
+@eel.expose
+def allCommands():
+
+    query = takecommand()
+    print(query)
+
+    if "open" in query:
+        from engine.features import openCommand
+        openCommand(query)
+    elif "on youtube":
+        from engine.features import PlayYoutube
+        PlayYoutube(query)
+        
+    else:
+        print("not run")
+
+    eel.ShowHood()
--- www/main.js
+++ www/main.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
         eel.playAssistantSound()
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
-        eel.takecommand()()
+        eel.allCommands()()
         
     });
 });
